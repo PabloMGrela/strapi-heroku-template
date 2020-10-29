04d2454377d8ad40cae8f0e76530a4ac7fff56a4
Jacobo Rodriguez <poborp@gmail.com>
2020-10-29 13:45:07 +0100

    Move secret

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', 'e1SIv1IxOLNexxRO7LmakGv6rsdMwPuikYJjefYPRaifXOQCUY1lbKv8YNbHQuOsoUCKc70OE4e3X4rHXALLGQ=='),
     },
   }
 });
